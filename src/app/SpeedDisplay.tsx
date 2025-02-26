bf476fad9600e95b8ba2ea6a8a945f6459724048
Marcos Camargo <121471879+marcoscamarg0@users.noreply.github.com>
2025-02-26 15:49:50 -0300

    h

--- src/app/SpeedDisplay.tsx
+++ src/app/SpeedDisplay.tsx
@@ -1,12 +1,13 @@
+// SpeedDisplay.tsx
 import React from 'react';
 import { View, Text } from 'react-native';
 import tw from 'twrnc';
 
 interface SpeedDisplayProps {
-  speed: number;
+  readonly speed: number;
 }
 
-const SpeedDisplay = ({ speed }: SpeedDisplayProps) => {
+const SpeedDisplay: React.FC<SpeedDisplayProps> = ({ speed }) => {
   return (
     <View style={tw`items-center`}>
       <Text style={tw`text-orange-500 text-3xl font-bold`}>{speed} KM/H</Text>
